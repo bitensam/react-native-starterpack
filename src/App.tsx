be9eb920fe77605f30f56193a273a25ab0b69da5
Jakub Wójcik <jakub.wojcik@ppuc.pl>
2023-06-15 10:30:20 +0200

    chore: inited project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider } from 'native-base';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
